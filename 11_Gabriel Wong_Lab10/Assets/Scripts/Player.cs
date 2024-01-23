acd99c4f889d6f66c293f7820194797f23ae3bbe
Gabriel <gabwong9999@gmail.com>
2024-01-23 16:05:40 +0800

    Added and completed Lab 10
    ~ by Gabriel

---
+++ 11_Gabriel Wong_Lab10/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -2.95f, 4.93f), transform.position.z);
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
